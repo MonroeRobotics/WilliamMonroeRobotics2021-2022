9ac7a00ae2532a7a4c3d140b4a081e732e5a7fa8
RobotNatchos <robot6895games@gmail.com>
2021-10-17 19:31:45 -0400

    Change, Adding Image Rec, Reorganizing Folder

--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ImageRec.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ImageRec.java
@@ -1,6 +1,5 @@
-package org.firstinspires.ftc.robotcontroller.external.samples;
+package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -15,39 +14,15 @@ import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackableDefaultListener;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
 
-/**
- * This OpMode illustrates using a webcam to locate and drive towards ANY Vuforia target.
- * The code assumes a basic two-wheel Robot Configuration with motors named left_drive and right_drive.
- * The motor directions must be set so a positive drive goes forward and a positive turn rotates to the right.
- *
- * Under manual control, the left stick will move forward/back, and the right stick will turn left/right.
- * This is called POV Joystick mode, different than Tank Drive (where each joystick controls a wheel).
- * Manually drive the robot until it displays Target data on the Driver Station.
- * Press and hold the *Left Bumper* to enable the automatic "Drive to target" mode.
- * Release the Left Bumper to return to manual driving mode.
- *
- * Use DESIRED_DISTANCE to set how close you want the robot to get to the target.
- * Speed and Turn sensitivity can be adjusted using the SPEED_GAIN and TURN_GAIN constants.
- *
- * For more Vuforia details, or to adapt this OpMode for a phone camera, view the
- *  ConceptVuforiaFieldNavigation and ConceptVuforiaFieldNavigationWebcam samples.
- *
- * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list.
- *
- * IMPORTANT: In order to use this OpMode, you need to obtain your own Vuforia license key as
- * is explained below.
- */
 
-@TeleOp(name="Drive To Target", group = "Concept")
-@Disabled
-public class ConceptVuforiaDriveToTargetWebcam extends LinearOpMode
-{
+@TeleOp(name="DriveToPicture", group = "Concept")
+
+public class ImageRec extends LinearOpMode {
     // Adjust these numbers to suit your robot.
     final double DESIRED_DISTANCE = 8.0; //  this is how close the camera should get to the target (inches)
-                                         //  The GAIN constants set the relationship between the measured position error,
-                                         //  and how much power is applied to the drive motors.  Drive = Error * Gain
-                                         //  Make these values smaller for smoother control.
+    //  The GAIN constants set the relationship between the measured position error,
+    //  and how much power is applied to the drive motors.  Drive = Error * Gain
+    //  Make these values smaller for smoother control.
     final double SPEED_GAIN =   0.02 ;   //  Speed Control "Gain". eg: Ramp up to 50% power at a 25 inch error.   (0.50 / 25.0)
     final double TURN_GAIN  =   0.01 ;   //  Turn Control "Gain".  eg: Ramp up to 25% power at a 25 degree error. (0.25 / 25.0)
 
@@ -65,23 +40,27 @@ public class ConceptVuforiaDriveToTargetWebcam extends LinearOpMode
      * Once you've obtained a license key, copy the string from the Vuforia web site
      * and paste it in to your code on the next line, between the double quotes.
      */
-    private static final String VUFORIA_KEY =
-            " --- YOUR NEW VUFORIA KEY GOES HERE  --- ";
+
+    private static final String VUFORIA_KEY = "AYgFQEn/////AAABmZktkoBTAkVNt+reKSY8LhArtiNgmXoPcLLAfyChoAu8zFN33UltlJ5qSzMikTirl0dLSkgn8Jix4iFSMbc3ArlSBwmNgP5nMSuEGCLvdjgLddPa7eBL7M8CrYcWGwG9E/snaBn5br/bkKQFsfdb334YxxMPn6EDfJplvsX+87KHhIKqKMsAKry4+PegiTwSyzOZapdb+qUmngVHVof2xObB2YI3TOikP2/T7qqToI+RpxVxV1ndg7yUCwTwAM2TjZUgj5N7uNV6dkAtt8+sYjhdXA5ZmsXzUm1a8BatDt8SkI6tXUEDohxLWGlwqdy4hyESnCX/fsglF43c2637MoRsqjjfeI7Nb3OGRMtOiI6A";
 
     VuforiaLocalizer vuforia    = null;
     OpenGLMatrix targetPose     = null;
     String targetName           = "";
 
-    private DcMotor leftDrive   = null;
-    private DcMotor rightDrive  = null;
+    private DcMotor fRight   = null;
+    private DcMotor fLeft  = null;
+    private DcMotor bRight  = null;
+    private DcMotor bLeft  = null;
 
-    @Override public void runOpMode()
+    @Override
+    public void runOpMode()
     {
         /*
          * Configure Vuforia by creating a Parameter object, and passing it to the Vuforia engine.
          * To get an on-phone camera preview, use the code below.
          * If no camera preview is desired, use the parameter-less constructor instead (commented out below).
          */
+
         int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
         VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
         // VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters();
@@ -108,13 +87,13 @@ public class ConceptVuforiaDriveToTargetWebcam extends LinearOpMode
         // Initialize the hardware variables. Note that the strings used here as parameters
         // to 'get' must correspond to the names assigned during the robot configuration
         // step (using the FTC Robot Controller app on the phone).
-        leftDrive  = hardwareMap.get(DcMotor.class, "left_drive");
-        rightDrive = hardwareMap.get(DcMotor.class, "right_drive");
+        fRight  = hardwareMap.get(DcMotor.class, "frontRight");
+        fLeft = hardwareMap.get(DcMotor.class, "frontLeft");
+        bRight = hardwareMap.get(DcMotor.class, "backRight");
+        bLeft = hardwareMap.get(DcMotor.class, "backLeft");
 
         // To drive forward, most robots need the motor on one side to be reversed, because the axles point in opposite directions.
         // Pushing the left stick forward MUST make robot go forward. So adjust these two lines based on your first test drive.
-        leftDrive.setDirection(DcMotor.Direction.FORWARD);
-        rightDrive.setDirection(DcMotor.Direction.REVERSE);
 
         telemetry.addData(">", "Press Play to start");
         telemetry.update();
@@ -126,6 +105,9 @@ public class ConceptVuforiaDriveToTargetWebcam extends LinearOpMode
         double  targetBearing   = 0;        // Robot Heading, relative to target.  Positive degrees means target is to the right.
         double  drive           = 0;        // Desired forward power (-1 to +1)
         double  turn            = 0;        // Desired turning power (-1 to +1)
+        double magnitude = .8;
+        double rightDrive = 0;
+        double leftDrive = 0;
 
         while (opModeIsActive())
         {
@@ -161,7 +143,6 @@ public class ConceptVuforiaDriveToTargetWebcam extends LinearOpMode
 
             // Tell the driver what we see, and what to do.
             if (targetFound) {
-                telemetry.addData(">","HOLD Left-Bumper to Drive to Target\n");
                 telemetry.addData("Target", " %s", targetName);
                 telemetry.addData("Range",  "%5.1f inches", targetRange);
                 telemetry.addData("Bearing","%3.0f degrees", targetBearing);
@@ -170,18 +151,40 @@ public class ConceptVuforiaDriveToTargetWebcam extends LinearOpMode
             }
 
             // Drive to target Automatically if Left Bumper is being pressed, AND we have found a target.
-            if (gamepad1.left_bumper && targetFound) {
+            if (targetFound) {
 
                 // Determine heading and range error so we can use them to control the robot automatically.
                 double  rangeError   = (targetRange - DESIRED_DISTANCE);
                 double  headingError = targetBearing;
 
                 // Use the speed and turn "gains" to calculate how we want the robot to move.
-                drive = rangeError * SPEED_GAIN;
+                magnitude = rangeError * SPEED_GAIN;
                 turn  = headingError * TURN_GAIN ;
 
                 telemetry.addData("Auto","Drive %5.2f, Turn %5.2f", drive, turn);
+
+                if (rangeError > 0) {
+                    if (headingError < 0){
+                        rightDrive = (magnitude);
+                        leftDrive = (magnitude - turn);
+                    }
+                    else if (headingError > 0)
+                        rightDrive = (magnitude - turn);
+                    leftDrive = (magnitude);
+                }
+                else {
+                    rightDrive = 0;
+                    leftDrive = 0;
+                }
             }
+
+
+
+
+            telemetry.addData("rightDrive", rightDrive);
+            telemetry.addData("leftDrive", leftDrive);
+
+
 
             telemetry.update();
 
---
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MechWhellTesdt.java
@@ -0,0 +1,77 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@TeleOp
+// Wheel Movement
+public class MechWhellTesdt extends LinearOpMode{
+    private DcMotor frontRight;
+    private DcMotor frontLeft;
+    private DcMotor backRight;
+    private DcMotor backLeft;
+    // todo: write your code here
+
+
+    public void runOpMode(){
+        double leftstickX;
+        double leftstickY;
+        double rightstickX;
+        double rightstickY;
+        double direction;
+        double magnitude;
+        double fRightBLeft;
+        double fLeftBRight;
+        double turn;
+
+        backLeft = hardwareMap.get(DcMotor.class, "backLeft");
+        backRight = hardwareMap.get(DcMotor.class, "backRight");
+        frontRight = hardwareMap.get(DcMotor.class, "frontRight");
+        frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+        waitForStart();
+
+        while (opModeIsActive()){
+
+            leftstickX = this.gamepad1.left_stick_x;
+            leftstickY = -this.gamepad1.left_stick_y;
+
+            turn = this.gamepad1.right_stick_x;
+
+            direction = Math.atan2(leftstickY, leftstickX);
+            magnitude = Math.sqrt(Math.pow(leftstickX, 2) + Math.pow(leftstickY, 2));
+
+            fRightBLeft = (Math.sin(direction - 1.0/4.0 * Math.PI) * magnitude + turn);
+            fLeftBRight = (Math.sin(direction + 1.0/4.0 * Math.PI) * magnitude + turn);
+
+            if (fRightBLeft > 1 || fRightBLeft < -1){
+                fRightBLeft = (fRightBLeft / Math.abs(fRightBLeft));
+                fLeftBRight = (fLeftBRight / Math.abs(fRightBLeft));
+            }
+
+            if (fLeftBRight > 1 || fLeftBRight < -1){
+                fLeftBRight = (fLeftBRight / Math.abs(fLeftBRight));
+                fRightBLeft = (fRightBLeft / Math.abs(fLeftBRight));
+            }
+
+            backLeft.setPower(-fRightBLeft);
+            frontRight.setPower(fRightBLeft);
+            backRight.setPower(fLeftBRight);
+            frontLeft.setPower(-fLeftBRight);
+
+            telemetry.addData("Status", "Initialized");
+            telemetry.addData("leftX", leftstickX);
+            telemetry.addData("leftY", leftstickY);
+            telemetry.addData("Direction", direction);
+            telemetry.addData("magnitude", magnitude);
+            telemetry.addData("fRightBleft", fRightBLeft);
+            telemetry.addData("fRightBleft", fLeftBRight);
+            telemetry.update();
+
+        }
+    }
+
+}
